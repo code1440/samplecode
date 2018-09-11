8564ac21c3aa763371bde61f641539243a8c1f71
code1440 <42753561+code1440@users.noreply.github.com>
2018-09-11 14:28:18 -0500

    Create ec2-create-instance.py

---
+++ ec2-create-instance.py
@@ -0,0 +1,17 @@
+import boto3
+
+
+def create_instance():
+    ec2_resource = boto3.resource('ec2')
+    instances = ec2_resource.create_instances(ImageId='ami-6871a115',
+                MinCount=1, MaxCount=3,InstanceType='t2.micro',
+                SecurityGroupIds=['sep06'],KeyName='fullstack')
+    instance_ids = []
+    for instance in instances:
+        instance_ids.append(instance.id)
+    ec2_client = boto3.client('ec2')
+    waiter=ec2_client.get_waiter('instance_running')
+    waiter.wait(InstanceIds=instance_ids)
+    print ("Instance is Running now!")
+
+create_instance()
